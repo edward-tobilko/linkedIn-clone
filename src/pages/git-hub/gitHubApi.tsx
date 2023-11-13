6717e4e549ad895af139b8d76940e1cc7cb5c41e
edward.tobilko <eduard777@gmail.com>
2023-11-13 19:56:06 +0200

    remove bearer token

--- src/pages/git-hub/gitHubApi.tsx
+++ src/pages/git-hub/gitHubApi.tsx
@@ -4,7 +4,7 @@ export const instance = axios.create({
   baseURL: "https://api.github.com/",
   headers: {
     "X-GitHub-Api-Version": "2022-11-28",
-    Authorization:
-      "Bearer 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",
+    // Authorization:
+    //   "Bearer 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",
   }, //? Для скасування ліміту запитів на сервер
 });
