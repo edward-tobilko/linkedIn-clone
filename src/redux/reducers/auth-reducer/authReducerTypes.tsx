0c8343c26b30d758e1236b0f6df0390b7b71970a
edward.tobilko <eduard777@gmail.com>
2023-11-20 18:11:40 +0200

    added some notes

--- src/redux/reducers/auth-reducer/authReducerTypes.tsx
+++ src/redux/reducers/auth-reducer/authReducerTypes.tsx
@@ -3,14 +3,14 @@ import { ThunkAction } from "redux-thunk";
 import { RootState, TypedActions } from "../../store";
 import { actions } from "./authReducer";
 
-// Type for actions
+//? Type for actions
 type AuthActionsTypes = TypedActions<typeof actions>;
 
-// Type for thunks
+//? Type for thunks
 type AuthThunkType<ReturnType = void> = ThunkAction<
   ReturnType,
   RootState,
-  unknown, // extra arguments
+  unknown, //? Extra arguments
   AuthActionsTypes
 >;
 
